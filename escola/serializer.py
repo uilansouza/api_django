f02084c67c5f569c2be6d61efa2d19a0ab556fcb
Uilan <uilan.souza@hotmail.com>
2021-11-18 22:58:58 -0300

    api funcionando

---
+++ escola/serializer.py
@@ -0,0 +1,28 @@
+from django.db.models import fields
+from rest_framework import serializers
+from escola.models import Aluno, Curso, Matricula
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id','nome','rg','cpf','data_nascimento']
+
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
+
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso','periodo']
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
---
+++ escola/views.py
@@ -0,0 +1,27 @@
+from django.views import generic
+from rest_framework import viewsets, generics, serializers
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os alunos e alunas""" 
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os cursos""" 
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """ Listando todas as Matriculas""" 
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matriculas de um aluno/aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
